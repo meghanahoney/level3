bbe83eed3b1adc51ae6728c4e30678717aa6dd39
meghanahoney <113693788+meghanahoney@users.noreply.github.com>
2022-11-02 22:25:41 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const firstName = () => console.log('Meghana');
-const lastName = () => console.log('Reddy');
-const printName = () => {
-    console.log('My name is:');
-    setTimeout(firstName,0);
-    lastName();
-}
-printName();
